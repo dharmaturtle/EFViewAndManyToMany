bff536503ba768ce9e2a08e9c31dd141483965e6
A <dharmaturtle@gmail.com>
2020-01-14 19:48:57 -0600

    Added a passing test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,11 +1,13 @@
 using System;
+using System.Linq;
 using Xunit;
 
 namespace EFViewAndManyToMany {
   public class UnitTest1 {
     [Fact]
     public void Test1() {
-
+      var context = new EFViewAndManyToManyDb();
+      context.PostView.ToList();
     }
   }
 }
