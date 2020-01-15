8a058d79908f48023f41aafb7c41f19f460718eb
A <dharmaturtle@gmail.com>
2020-01-14 19:48:06 -0600

    Added Author to PostVIew

--- PostView.cs
+++ PostView.cs
@@ -6,5 +6,8 @@ namespace EFViewAndManyToMany
     public partial class PostView
     {
         public int Id { get; set; }
+        public int AuthorId { get; set; }
+
+        public virtual Author Author { get; set; }
     }
 }
